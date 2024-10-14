19b2750c1133a73457acd630d068b34c35108978
Yannis Bianchini Pontuschka <56230345+YannisPontuschka@users.noreply.github.com>
2024-10-13 22:03:48 -0300

    fix: usign HashRouter instead of Browser

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home"
+import { HashRouter } from "react-router-dom";
+
+import Home from "./components/Home";
 import SingleGame from "./components/SingleGame"; // Importando o componente SingleGame
 import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home/>}/>
+          <Route path="/" element={<Home />} />
           <Route path="/game/:gameId" element={<SingleGame />} />{" "}
           {/* Rota para detalhes do jogo */}
         </Routes>
       </div>
-    </Router>
+    </HashRouter>
   );
 }
 
